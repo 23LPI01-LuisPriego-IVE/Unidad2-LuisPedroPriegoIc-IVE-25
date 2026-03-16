852ddd5b815547c2c620ace37d7f2a6b45771ef9
23LPI01-LuisPriego-IVE <lpriegodb23@gmail.com>
2026-03-16 16:46:36 -0600

    Add files via upload

---
+++ EjemploIfElse.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Numero negativos y positivos");
+        int num;
+        Console.WriteLine("Dame un numero entero");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero >= 0)
+        {
+            Console.WriteLine("Soy un numero positivo");
+        }
+        else
+        {
+            Console.WriteLine("Soy un numero negativo");
+        }
+    }
+}
---
+++ EjemploSiEntonces.psc
@@ -0,0 +1,16 @@
+Algoritmo EjemploSiEntonces
+	//Entrada
+	Definir num Como Entero
+	Escribir "Dame un numero entero: "
+	Leer num
+	
+	//Proceso
+	
+	Si (num >= 0) Entonces
+		Escribir "Soy un numero positivo"
+	SiNo
+		Escribir "Soy un numero negativo"
+	Fin Si
+	
+	//Salida
+FinAlgoritmo
