f905ab201315b13fc280cbe6a947f11074fb613a
Luis Pedro Priego Ic <lpriegodb23@gmail.com>
2026-03-26 19:51:09 -0600

    Add files via upload

---
+++ Act1_Lecc7_inciso1.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double n1, n2, n3, n4, n5, prom;
+
+        Console.WriteLine("Luis Priego-IV-E");
+        Console.WriteLine("Ingrese 5 calificaciones:");
+
+        n1 = Convert.ToDouble(Console.ReadLine());
+        n2 = Convert.ToDouble(Console.ReadLine());
+        n3 = Convert.ToDouble(Console.ReadLine());
+        n4 = Convert.ToDouble(Console.ReadLine());
+        n5 = Convert.ToDouble(Console.ReadLine());
+
+        prom = (n1 + n2 + n3 + n4 + n5) / 5;
+
+        if (prom >= 70)
+        {
+            Console.WriteLine("Aprueba");
+        }
+        else
+        {
+            Console.WriteLine("Reprueba");
+        }
+
+    }
+}
---
+++ Act1_Lecc7_inciso1.psc
@@ -0,0 +1,15 @@
+Algoritmo  Act1_Lecc7_inciso1
+	Definir n1,n2,n3,n4,n5,prom Como Real
+	
+	Escribir "Luis Priego-IV-E"
+	Escribir "Ingrese 5 calificaciones:"
+	Leer n1,n2,n3,n4,n5
+	
+	prom = (n1+n2+n3+n4+n5) / 5
+	
+	Si prom >= 70 Entonces
+		Escribir "Aprueba"
+	SiNo
+		Escribir "Reprueba"
+	FinSi
+FinAlgoritmo
