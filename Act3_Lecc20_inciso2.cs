f70907acd1ec65cd226321c4354c152b247bc14a
Luis Pedro Priego Ic <lpriegodb23@gmail.com>
2026-03-26 20:15:09 -0600

    Add files via upload

---
+++ Act3_Lecc20_inciso2.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int nota;
+
+        Console.WriteLine("Luis Priego-IV-E");
+        Console.WriteLine("Ingrese la calificacion:");
+
+        nota = Convert.ToInt32(Console.ReadLine());
+
+        if (nota >= 70)
+        {
+            Console.WriteLine("Aprobado");   
+        }
+        else
+        {
+            Console.WriteLine("Reprobado");
+        }
+
+    }
+}
---
+++ Act3_Lecc20_inciso2.psc
@@ -0,0 +1,13 @@
+Algoritmo Act3_Lecc20_inciso2
+	Definir nota Como Entero
+	
+	Escribir "Luis Priego-IV-E"
+	Escribir "Ingrese la calificacion:"
+	Leer nota
+	
+	Si nota >= 70 Entonces
+		Escribir "Aprobado"
+	SiNo
+		Escribir "Reprobado"
+	FinSi
+FinAlgoritmo
