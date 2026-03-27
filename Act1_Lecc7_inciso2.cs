74651d565401255766f88c57da1b19bfe2c477ad
Luis Pedro Priego Ic <lpriegodb23@gmail.com>
2026-03-26 19:53:39 -0600

    Add files via upload

---
+++ Act1_Lecc7_inciso2.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num;
+
+        Console.WriteLine("Luis Priego-IV-E");
+        Console.WriteLine("Ingrese su edad:");
+
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if (num >= 18)
+        {
+            Console.WriteLine("Mayor de edad");
+        }
+        else
+        {
+            Console.WriteLine("Menor de edad");
+        }
+
+    }
+}
---
+++ Act1_Lecc7_inciso2.psc
@@ -0,0 +1,13 @@
+Algoritmo  Act1_Lecc7_inciso2
+	Definir num Como Entero
+	
+	Escribir "Luis Priego-IV-E"
+	Escribir "Ingrese su edad:"
+	Leer num
+	
+	Si num >= 18 Entonces
+		Escribir "Mayor de edad"
+	SiNo
+		Escribir "Menor de edad"
+	FinSi
+FinAlgoritmo
