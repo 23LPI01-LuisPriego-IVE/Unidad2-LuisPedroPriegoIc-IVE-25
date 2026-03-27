80b0f8b7ed24365cfc484c9f81da61de411c8dc3
Luis Pedro Priego Ic <lpriegodb23@gmail.com>
2026-03-26 20:10:25 -0600

    Add files via upload

---
+++ Act2_Lecc20_inciso1.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string pass;
+
+        Console.WriteLine("Luis Priego-IV-E");
+        Console.WriteLine("Ingrese la contraseña:");
+        pass = Console.ReadLine();
+
+        if (pass == "Password123")
+        {
+            Console.WriteLine("Bienvenid@");
+        }
+        else
+        {
+            Console.WriteLine("Acceso denegado");
+        }
+
+    }
+}
---
+++ Act2_Lecc20_inciso1.psc
@@ -0,0 +1,13 @@
+Algoritmo Act2_Lecc20_inciso1
+	Definir pass Como Cadena
+	
+	Escribir "Luis Priego-IV-E"
+	Escribir "Ingrese la contraseña:"
+	Leer pass
+	
+	Si pass = "Password123" Entonces
+		Escribir "Bienvenid@"
+	SiNo
+		Escribir "Acceso denegado"
+	FinSi
+FinAlgoritmo
